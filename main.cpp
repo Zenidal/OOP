b96f4d9dddf576456b8c9f1d1cd69187c9119c7a
Zenidal <1ochka1994@mail.ru>
2021-11-07 15:34:16 +0300

    task 1 and 2

--- main.cpp
+++ main.cpp
@@ -1,43 +1,49 @@
 #include <iostream>
+#include <limits>
 
 #include "Homework2/Task1/Student.h"
 
-#include "Homework5/Task1/Pair1.h"
-#include "Homework5/Task1/Pair1.cpp" // to avoid link errors
-#include "Homework5/Task2/Pair.h"
-#include "Homework5/Task2/Pair.cpp" // to avoid link errors
-#include "Homework5/Task3/StringValuePair.h"
-
 short Student::studentsCounter = 0;
+
+std::ostream &endll(std::ostream &os)
+{
+    os << "\n\n";
+
+    os.flush();
+
+    return os;
+}
 
 int main()
 {
     { // task 1
-        Pair1<int> p1(6, 9);
-        std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+        int value;
+        bool isValidValue = false;
 
-        const Pair1<double> p2(3.4, 7.8);
-        std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+        do {
+            std::cout << "Please enter integer value: " << std::endl;
+            std::cin >> value;
 
+            if (std::cin.good()) {
+                isValidValue = true;
+            } else {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+                isValidValue = false;
+
+                std::cerr << value << " is not valid integer value." << std::endl;
+            }
+        } while (!isValidValue);
     }
 
     std::cout << std::endl << std::endl;
 
     { // task 2
-        Pair<int, double> p1(6, 7.8);
-        std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
-
-        const Pair<double, int> p2(3.4, 5);
-        std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
-
+        std::cout << "Line 1" << endll << "Line 2" << std::endl;
     }
 
     std::cout << std::endl << std::endl;
-
-    { // task 3
-        StringValuePair<int> svp("Amazing", 7);
-        std::cout << "Pair: " << svp.first() << ' ' << svp.second() << '\n';
-    }
 
     return 0;
 }
